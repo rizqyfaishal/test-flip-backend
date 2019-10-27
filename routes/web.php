1d2188c7114e3f345b1566723783e2dfd8dabf22
Rizqy Faishal <rizqyfaishal27@gmail.com>
2019-10-27 09:39:25 +0700

    remove unneccessary code and update readme

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Controllers\DisburseController;
 
 $router = new Router();
 
-$router->get('/\/users$/', UserController::class, 'index');
 $router->get('/\/disbursements$/', DisburseController::class, 'index');
 $router->post('/\/disbursements$/', DisburseController::class, 'store');
 $router->get('/\/disbursements\/([0-9]+)$/', DisburseController::class, 'detail');
